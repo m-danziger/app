a20562c91e84c117fc7cf3a37e67197519813ebf
mdanziger <7197888@gmail.com>
2024-06-10 14:26:56 -0400

    06102024

--- remove.js
+++
@@ -1,7 +0,0 @@
-function taskRemove(btnRemove) {
-    if (confirm("Are you sure to delete?") == true){
-    let id = btnRemove.parentNode.parentNode.getAttribute('data-task-id');
-    tasks.splice(id, 1);
-    displayTasks();
-    }
-  }
