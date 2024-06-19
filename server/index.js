04f935fe104737804e07892c0e5a49f752cf8058
m-danziger <7197888@gmail.com>
2024-06-19 19:06:55 -0400

    06192024

--- server/index.js
+++ server/index.js
@@ -25,16 +25,35 @@ const db = pgp({
  })*/
 
 app.post('/tasks', async (req, res) => {
-    // tasks.push(req.body)
-    const b = await db.one('insert into todos.task (title, user_id) values (${title}, ${user_id}) returning *', {
-        title: req.body.title,
-        user_id: 1
-    })
-    res.json({
-        title: b.title,
-        done: false,
-        id: b.user_id
-    })
+    try {
+        // tasks.push(req.body)
+        const userId = req.query.userIdLogin;
+        const b = await db.one('insert into todos.task (title, user_id) values (${title}, ${user_id}) returning *', {
+            title: req.body.title,
+            user_id: userId
+        })
+        res.json({
+            title: b.title,
+            done: false,
+            id: b.user_id
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'tasks post failed' })
+    }
+})
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const userId = req.query.userIdLogin;
+        let a = await db.any('select * from todos.task where user_id = ${idUser} AND deleted_at is null', {
+            "idUser": userId
+        })
+        res.json(a.map(task => ({ id: task.id, title: task.title, done: task.status !== 'active' })))
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'tasks get failed' })
+    }
 })
 
 app.post('/users', async (req, res) => {
@@ -82,32 +101,37 @@ app.get('/users', async (req, res) => {
         res.json(a)
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: 'users Login failed' })
+        res.status(500).json({ error: 'users get failed' })
     }
 })
 
-app.get('/tasks', async (req, res) => {
-    const userId = req.query.userIdLogin;
-    let a = await db.any('select * from todos.task where user_id = ${idUser} AND deleted_at is null', {
-        "idUser": userId
-    })
-    res.json(a.map(task => ({ id: task.id, title: task.title, done: task.status !== 'active' })))
-})
 
 app.patch('/tasks/:id', async (req, res) => {
-    // let task = tasks.find((task) => task.id.toString() === req.body.id)
-    await db.any("update todos.task SET status = 'done' where id = ${id}", {
-        id: req.params.id
-    })
-    //task.done = true;
-    res.json()
+    try {
+
+
+        // let task = tasks.find((task) => task.id.toString() === req.body.id)
+        await db.any("update todos.task SET status = 'done' where id = ${id}", {
+            id: req.params.id
+        })
+        //task.done = true;
+        res.json()
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'tasks patch failed' })
+    }
 })
 
 app.delete('/tasks/:id', async (req, res) => {
-    await db.any("update todos.task SET deleted_at = now() where id = ${id}", {
-        id: req.params.id
-    })
-    res.json()
+    try {
+        await db.any("update todos.task SET deleted_at = now() where id = ${id}", {
+            id: req.params.id
+        })
+        res.json()
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'tasks delete failed' })
+    }
 })
 
 app.listen('3000', () => {
